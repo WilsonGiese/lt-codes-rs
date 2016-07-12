d92809f265245fea53f43c397c466cf7240d4e3c
Wilson Giese <giese.wilson@gmail.com>
2016-07-11 22:05:13 -0700

    Move structs to lib. Add lifetimes & make public

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
 #[macro_use]
 mod encoder;
-mod decoder; 
+mod decoder;
+
+pub struct Packet {
+    header: Header,
+    block: RawEncodedBlock,
+}
+
+pub struct Header {
+    // Size of block in bytes
+    block_size: u64,
+    // Size of source in bytes (Final block will be padded?)
+    source_size: u64,
+}
+
+///
+pub struct RawEncodedBlock {
+    // Encoded data
+    data: Vec<u8>,
+    // Seed for PRNG
+    seed: u64,
+}
+
+pub struct EncodedBlock {
+    // Encoded data (until len(sources) == 1)
+    data: Vec<u8>,
+    // Set of source block indicies which makeup the encoded data
+    sources: Vec<u64>,
+}
+
+pub struct SourceBlock<'a> {
+    // Decoded data
+    data: Vec<u8>,
+    // Pointers to encoded blocks which encode this source block
+    encoded_blocks: Vec<&'a EncodedBlock>,
+}
+
+pub struct Source<'a> {
+    // Entirety of decoded source data
+    data: Vec<&'a SourceBlock<'a>>,
+}
